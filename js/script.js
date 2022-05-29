6a312d8c33c1bcb46be6c949ee50981a49b8a7bd
Mariuskvaal <mariuskvaal1@gmail.com>
2022-05-29 22:48:29 +0200

    lesson 3 done almost

---
+++ js/script.js
@@ -0,0 +1,65 @@
+//Question 1 
+//
+var firstNumber = 34;
+var secondNumber = 89;
+
+if (firstNumber < secondNumber) {
+    console.log("The first number is smaller than the second")
+}
+
+//Question 2 
+//
+
+var age = 11;
+var minimumAge = "13";
+
+if (age >= minimumAge) {
+    console.log("Your age meets the requirements")
+} else {
+    console.log("Your age does not meet the minimum age requirments")
+}
+
+//Question 3
+//
+
+var income = "11.050";
+var maximumIncome = "13.075";
+
+var convertingStringToNumbers = parseFloat(income)
+
+var convertingStringToNumbers2 = parseFloat(maximumIncome)
+
+
+if (convertingStringToNumbers < convertingStringToNumbers2) {
+    console.log("your income is less then the income requirments");
+} else {
+    concole.log("your income is above the maximum income level");
+}
+
+//Question 4
+//
+
+var colour = "blue";
+
+if (colour = "orange") {
+    console.log("This colour is a bit rubbish");
+}
+
+//Question 5
+//
+
+var invoicePaid = false;
+
+if (invoicePaid === false) {
+    console.log("Not Paid");
+}
+
+//Question 6
+//
+
+var selectedNumber = 11;
+var winningNumber = "15";
+
+
+
+
